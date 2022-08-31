5f21cc83caf879c8f6986d0d3574b031e430b4a5
Enrique Hernandez <enrique_inf@hotmail.com>
2022-08-30 20:33:23 -0500

    agregando contexto

---
+++ TareasContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projectoef.Models;
+
+namespace projectoef
+{
+    public class TareasContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options): base(options)
+        {
+
+        }
+    }
+}
