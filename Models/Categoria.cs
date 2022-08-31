40e24773e9be4ee858bacbf2f0346bd5085cef04
Enrique Hernandez <entique_inf@hotmail.com>
2022-08-31 11:57:51 -0500

    mapeo de modelos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,14 @@
-﻿namespace projectoef.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projectoef.Models
 {
     public class Categoria
     {
+        [Key]
         public Guid CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public virtual ICollection<Tarea> Tareas { get; set; }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,15 +1,27 @@
-﻿namespace projectoef.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectoef.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
+
+        [ForeignKey("CagegoriaId")]
         public Guid CategoriaId{ get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Prioridad PrioridadTarea { get; set; }
         public DateTime FechaCreacion { get; set; }
 
         public virtual Categoria Categroia { get; set; }
+
+        [NotMapped]
+        public string Resumen { get; set; }
 
     }
 
