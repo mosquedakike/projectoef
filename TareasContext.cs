8d4c00b83822885aeaf498d48e06944a1084f676
Enrique Hernandez <enrique_inf@hotmail.com>
2022-08-31 21:32:36 -0500

    finalizando modelo tarea con Fluen API

--- TareasContext.cs
+++ TareasContext.cs
@@ -19,6 +19,7 @@ namespace projectoef
             modelBuilder.Entity<Categoria>(categoria =>
             {
                 categoria.ToTable("Categoria");
+
                 categoria.HasKey(p => p.CategoriaId);
 
                 categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(200);
@@ -29,7 +30,18 @@ namespace projectoef
             modelBuilder.Entity<Tarea>(tarea =>
             {
                 tarea.ToTable("Tarea");
+
                 tarea.HasKey(p => p.TareaId);
+
+                tarea.HasOne(p => p.Categroia).WithMany(p => p.Tareas).HasForeignKey(p => p.CategoriaId);
+
+                tarea.Property(p => p.Titulo).IsRequired().HasMaxLength(200);
+
+                tarea.Property(p => p.Descripcion).HasMaxLength(200);
+
+                tarea.Property(p => p.PrioridadTarea);
+
+                tarea.Property(p => p.FechaCreacion);
 
             });
         }
